fd7dd83eb9ba10aa5022caeee4c4338600f1873e
bensku <bmi@iki.fi>
2019-09-05 21:55:16 +0300

    First commit

---
+++ src/main/java/io/github/bensku/deobf/ClassMapping.java
@@ -0,0 +1,15 @@
+package io.github.bensku.deobf;
+
+/**
+ * Contains class name and member name mappings.
+ *
+ */
+public class ClassMapping {
+
+    /**
+     * Fully qualified target name of this class.
+     */
+    private final String name;
+    
+    
+}
---
+++ src/main/java/io/github/bensku/deobf/Mappings.java
@@ -0,0 +1,12 @@
+package io.github.bensku.deobf;
+
+import java.util.Map;
+
+/**
+ * Maps class names
+ *
+ */
+public class Mappings {
+
+    private final Map<String, ClassMapping> classes;
+}
